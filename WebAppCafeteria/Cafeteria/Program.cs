ef1b0e04c0eed5bc5e9188418fd908819556a0f1
JulioFigueiredo <julioo.1amorim@gmail.com>
2024-10-24 13:03:16 -0300

    docs: update WebAppCafeteria

--- WebAppCafeteria/Cafeteria/Program.cs
+++ WebAppCafeteria/Cafeteria/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Cafeteria.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CafeteriaContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("CafeteriaContext") ?? throw new InvalidOperationException("Connection string 'CafeteriaContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +18,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
