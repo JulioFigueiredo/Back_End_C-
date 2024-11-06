3db743ce6b4e6cccb528757188ccc4ae98631965
JulioFigueiredo <julioo.1amorim@gmail.com>
2024-11-06 13:10:46 -0300

    docs: add API studies

---
+++ ExercicioAPI/CafeteriaAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+namespace CafeteriaAPI;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int Quantity { get; set; }
+
+    public string? Category { get; set; }
+
+    public decimal Price { get; set; }
+}
